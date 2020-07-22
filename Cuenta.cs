0ded30422f716eb2c80c1bcdc029525f497d8601
Katherin <Angel@DESKTOP-K23UOE7>
2020-07-22 08:37:49 -0400

    Add project files.

---
+++ Cuenta.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejericio2_Cuenta
+{
+    class Cuenta
+    {
+        private string t;
+        private int r, d, c = 3000;
+
+        public string titular
+        {
+            get
+            {
+                return t;
+            }
+
+            set
+            {
+                t = value;
+            }
+        }
+
+        public int cantidad
+        {
+            get
+            {
+                return c;
+            }
+
+            set
+            {
+                c = value;
+            }
+        }
+
+        public int retiro
+        {
+            get
+            {
+                return r;
+            }
+
+            set
+            {
+                r = value;
+            }
+        }
+
+        public int deposito
+        {
+            get
+            {
+                return d;
+            }
+
+            set
+            {
+                d = value;
+            }
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("- Informacion de Cuenta:");
+            Console.WriteLine(" ");
+            Console.WriteLine("Titular: {0}", titular);
+            Console.WriteLine("Cantidad: {0}", cantidad);
+            Console.WriteLine(" ");
+            Console.WriteLine("Presione la tecla <ENTER> para continuar.");
+            Console.ReadKey();
+        }
+
+        public void Ingresar()
+        {
+            Console.WriteLine(" ");
+            Console.Write("- Ingrese la cantidad que desea depositar a la Cuenta:");
+            deposito = Convert.ToInt32(Console.ReadLine());
+            cantidad = cantidad + deposito;
+            Console.WriteLine("Deposito: {0}", deposito);
+            Console.WriteLine("Cantidad: {0}", cantidad);
+            deposito = 0;
+            Console.WriteLine(" ");
+            Console.WriteLine("Presione la tecla <ENTER> para continuar.");
+            Console.ReadKey();
+        }
+
+        public void Retirar()
+        {
+            Console.WriteLine(" ");
+            Console.Write("- Ingrese la cantidad que desea retirar a la Cuenta:");
+            retiro = Convert.ToInt32(Console.ReadLine());
+            cantidad = cantidad - retiro;
+            Console.WriteLine("Retiro: {0}", retiro);
+            Console.WriteLine("Cantidad: {0}", cantidad);
+            retiro = 0;
+            Console.WriteLine(" ");
+            Console.WriteLine("Presione la tecla <ENTER> para continuar.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejericio2_Cuenta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta = new Cuenta();
+
+           
+
+
+            int menu = 1;
+            int opcion = 0;
+
+            Console.WriteLine("Ejercicio 2: Clase Cuenta.");
+            Console.WriteLine(" ");
+            Console.Write("- Digite el nombre del titular de la cuenta: ");
+            cuenta.titular = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            do
+            {
+                Console.WriteLine("Opciones del Sistema:");
+                Console.WriteLine(" ");
+                Console.WriteLine("1- Depositar.");
+                Console.WriteLine("2- Retirar.");
+                Console.WriteLine("3- Mostrar datos de la Cuenta.");
+                Console.WriteLine("4- Salir.");
+                Console.WriteLine(" ");
+                Console.Write("- Digite el numero de la opcion deseada: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        cuenta.Ingresar();
+                        break;
+                    case 2:
+                        cuenta.Retirar();
+                        break;
+                    case 3:
+                        cuenta.Mostrar();
+                        break;
+                    case 4:
+                        menu = 0;
+                        break;
+                }
+            }
+            while (menu != 0);
+        }
+    }
+}
